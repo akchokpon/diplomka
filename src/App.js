1ba53c60e079939a2311face8236bf6650d7fed5
akchokpon <148753794+akchokpon@users.noreply.github.com>
2024-05-15 12:57:55 +0600

    add menu

--- src/App.js
+++ src/App.js
@@ -20,15 +20,12 @@ function App() {
 
           <Routes>
             {/* home page route */}
-            <Route path = "/" element = {<Home />} />
-            {/* single product route */}
+            <Route path = "/" element = {<Home />} /> 
             <Route path = "/product/:id" element = {<ProductSingle />} />
-            {/* category wise product listing route */}
             <Route path = "/category/:category" element = {<CategoryProduct />} />
-            {/* cart */}
             <Route path = "/cart" element = {<Cart />} />
-            {/* searched products */}
             <Route path = "/search/:searchTerm" element = {<Search />} />
+            
           </Routes>
 
           <Footer />
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,68 +2,34 @@ import React from 'react';
 import "./Header.scss";
 import {Link} from 'react-router-dom';
 import Navbar from "../Navbar/Navbar";
+import Menu from "../Menu/Menu"
+import Button from "../Button/Button"
 
 const Header = () => {
   return (
     <header className='header text-white'>
       <div className='container'>
         <div className='header-cnt'>
-          <div className='header-cnt-top fs-13 py-2 flex align-center justify-between'>
-            <div className='header-cnt-top-l'>
-              <ul className='flex top-links align-center'>
-                <li>
-                  {/* dummy links */}
-                  <Link to = "/seller">Seller Center</Link>
-                </li>
-                <li className='vert-line'></li>
-                <li>
-                  {/* dummy links */}
-                  <Link to = "/download">Download</Link>
-                </li>
-                <li className='vert-line'></li>
-                <li className='flex align-center'>
-                  <span className='fs-13'>Follow us on</span>
-                  <ul className='social-links flex align-center'>
-                    <li className='mx-2'>
-                      <a href = "www.facebook.com" className='fs-15'>
-                        <i className='fab fa-facebook'></i>
-                      </a>
-                    </li>
-                    <li className='mx-2'>
-                      <a href = "www.instagram.com" className='fs-15'>
-                        <i className='fab fa-instagram'></i>
-                      </a>
-                    </li>
-                  </ul>
-                </li>
-              </ul>
-            </div>
-            <div className='header-cnt-top-r'>
-              <ul className='top-links flex align-center'>
-                <li>
-                  <Link to = "/" className='top-link-itm'>
-                    <span className='top-link-itm-ico mx-2'>
-                      <i className='fa-solid fa-circle-question'></i>
-                    </span>
-                    <span className='top-link-itm-txt'>Support</span>
-                  </Link>
-                </li>
-                <li className='vert-line'></li>
-                <li>
-                  <Link to = "/">
-                    <span className='top-link-itm-txt'>Register</span>
-                  </Link>
-                </li>
-                <li className='vert-line'></li>
-                <li>
-                  <Link to = "/">
-                    <span className='top-link-itm-txt'>Log in</span>
-                  </Link>
-                </li>
-              </ul>
-            </div>
+        <header className="Header">
+      <div className="container">
+        <div className="Head flex">
+         
+        <Link to = "/" className='navbar-brand flex align-center'>
+            <span className='navbar-brand-ico'>
+              <i className='fa-solid fa-bag-shopping'></i>
+            </span>
+            <span className='navbar-brand-txt mx-2'>
+              <span className='fw-7'>Choco</span>Pai.
+            </span>
+          </Link>
+          <Menu />
+          <div className="Buttons flex">
+            <Button>Login</Button>
+            <Button>Sign In</Button>
           </div>
-
+        </div>
+      </div>
+    </header>
           <div className='header-cnt-bottom'>
             <Navbar />
           </div>
